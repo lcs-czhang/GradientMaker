039fda713345bfca9ffdb4db590bcf5d80f12fba
Youzhou Zhang <czhang@lcs.on.ca>
2024-01-11 17:08:03 -0500

    Initial Commit

---
+++ GradientMaker/GradientMakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GradientMakerApp.swift
+//  GradientMaker
+//
+//  Created by Yuzhou Zhang on 2024-01-11.
+//
+
+import SwiftUI
+
+@main
+struct GradientMakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
