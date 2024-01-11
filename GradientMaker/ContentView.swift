bb3692c610de9919625e07dc62d1011b67ed0713
Youzhou Zhang <czhang@lcs.on.ca>
2024-01-11 17:14:11 -0500

    Made two colours

--- GradientMaker/ContentView.swift
+++ GradientMaker/ContentView.swift
@@ -8,12 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    //MARK: Stored property
+    @State var startingColor = Color.red
+    @State var endingColor = Color.blue
+    
+    //MARK: Computed property
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            LinearGradient(gradient: /*@START_MENU_TOKEN@*/Gradient(colors: [Color.red, Color.blue])/*@END_MENU_TOKEN@*/, startPoint: /*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/, endPoint: /*@START_MENU_TOKEN@*/.trailing/*@END_MENU_TOKEN@*/)
         }
         .padding()
     }
